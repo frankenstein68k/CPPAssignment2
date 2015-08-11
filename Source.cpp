03762738fc3be941eba40021e375839aafc4df31
frankenstein68k <frankenstein68k@gmail.com>
2015-08-11 14:04:51 -0400

    Starting new menu

--- Source.cpp
+++ Source.cpp
@@ -18,91 +18,102 @@ int main(){
 	int userResp = 1;
 	string optionTitle;
 
-	while (userResp == 1 || userResp == 2)
-	{
-		system("CLS");
+	char userName[20];
 
-		userResp = displayMainMenu();
 
-		system("CLS");
+	cout << "Welcome to the High Score Keeper App" << endl;
+	cout << "\n\tIf you are a new user, enter a username you would like (no spaces)";
+	cout << "\n\tPlease enter your Username (no spaces): ";
+	cin >> userName;
 
-		//user validation
-		while (userResp != 1 && userResp != 2)
-		{
-			cout << "Please choose 1 or 2" << endl;
-			userResp = displayMainMenu();
+	cout << "\nYou have entered " << userName << endl << endl;
 
-			system("CLS");
-		}
 
-		if (userResp == 1)
-		{
-			optionTitle = "view the User Information Section";
-			displayChoice(optionTitle);
+	//while (userResp == 1 || userResp == 2)
+	//{
+	//	system("CLS");
 
-			//User class functions
-			cout << "User information Section" << endl;
-			cout << "1. Login/Create new user" << endl;
-			cout << "2. Update/edit user" << endl;
-			cout << "3. Delete user" << endl;
-			cout << "4. Set high score" << endl;
-			cin >> userResp;
-			//User validation
-			while (userResp < 1 || userResp > 4){
-				cout << "Please choose a number between 1-4" << endl;
-				cin >> userResp;
-			}
-			//User confirmation
-			cout << "You have chosen " << userResp << endl;
+	//	userResp = displayMainMenu();
 
-			//Calls to user functions
-			if (userResp == 1)
-			{
+	//	system("CLS");
 
-			}
-			else if (userResp == 2)
-			{
-				//Updateuserlist
-			}
-			else if (userResp == 3)
-			{
-				//delete user
-			}
-			else if (userResp == 4)
-			{
-				//setHighScore
-			}
-		}
-		if (userResp == 2)
-		{
-			optionTitle = "view the High Score Manager";
-			displayChoice(optionTitle);
-			//User class functions
-			cout << "High Score Manager" << endl;
-			cout << "------------------" << endl;
-			cout << "1. Create high score" << endl;
-			cout << "2. Print high score" << endl;
-			cin >> userResp;
-		}
-		while (userResp < 1 || userResp > 3){
-			cout << "Please choose a number between 1-3" << endl;
-			cin >> userResp;
-		}
-		//User confirmation
-		cout << "You have chosen " << userResp << endl;
+	//	//user validation
+	//	while (userResp != 1 && userResp != 2)
+	//	{
+	//		cout << "Please choose 1 or 2" << endl;
+	//		userResp = displayMainMenu();
 
-		if (userResp == 1)
-		{
-				HighScoreManager HighScore;
-					HighScore.createHighScore();
-		}
-		else if (userResp == 2)
-		{
-			HighScoreManager HighScore;
-				HighScore.printHighScore();
-		}
+	//		system("CLS");
+	//	}
 
-	}
+	//	if (userResp == 1)
+	//	{
+	//		optionTitle = "view the User Information Section";
+	//		displayChoice(optionTitle);
+
+	//		//User class functions
+	//		cout << "User information Section" << endl;
+	//		cout << "1. Login/Create new user" << endl;
+	//		cout << "2. Update/edit user" << endl;
+	//		cout << "3. Delete user" << endl;
+	//		cout << "4. Set high score" << endl;
+	//		cin >> userResp;
+	//		//User validation
+	//		while (userResp < 1 || userResp > 4){
+	//			cout << "Please choose a number between 1-4" << endl;
+	//			cin >> userResp;
+	//		}
+	//		//User confirmation
+	//		cout << "You have chosen " << userResp << endl;
+
+	//		//Calls to user functions
+	//		if (userResp == 1)
+	//		{
+
+	//		}
+	//		else if (userResp == 2)
+	//		{
+	//			//Updateuserlist
+	//		}
+	//		else if (userResp == 3)
+	//		{
+	//			//delete user
+	//		}
+	//		else if (userResp == 4)
+	//		{
+	//			//setHighScore
+	//		}
+	//	}
+	//	if (userResp == 2)
+	//	{
+	//		optionTitle = "view the High Score Manager";
+	//		displayChoice(optionTitle);
+	//		//User class functions
+	//		cout << "High Score Manager" << endl;
+	//		cout << "------------------" << endl;
+	//		cout << "1. Create high score" << endl;
+	//		cout << "2. Print high score" << endl;
+	//		cin >> userResp;
+	//	}
+	//	while (userResp < 1 || userResp > 3){
+	//		cout << "Please choose a number between 1-3" << endl;
+	//		cin >> userResp;
+	//	}
+	//	//User confirmation
+	//	cout << "You have chosen " << userResp << endl;
+
+	//	if (userResp == 1)
+	//	{
+	//			HighScoreManager HighScore;
+	//				HighScore.createHighScore();
+	//	}
+	//	else if (userResp == 2)
+	//	{
+	//		HighScoreManager HighScore;
+	//			HighScore.printHighScore();
+	//	}
+
+	//}
 
 	system("PAUSE");
 	return 0;
